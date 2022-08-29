871dcbf1f64a81d768cca1c2f61fa0a9302fb81c
Jose Giraldo <wetdog@Joses-MacBook-Air.local>
2022-08-29 19:09:06 +0200

    Add start and end samples to Json data

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -10,7 +10,6 @@ import umap
 
 
 # load audio 
-
 audio_file = "../long_audio.wav"
 assert os.path.isfile(audio_file), "Invalid audio path"
 
@@ -33,17 +32,19 @@ def get_random_signal(n_seconds,sample_rate):
     return np.random.rand(sample_rate*n_seconds)
 
 sample_rate = 16000
-#x =  get_random_signal(n_seconds=40,sample_rate=sample_rate)
-x = load_audio_16khz(audio_file)
+hop_size = 0.48
+window_size = 0.96
+x =  get_random_signal(n_seconds=4,sample_rate=sample_rate)
+#x = load_audio_16khz(audio_file)
 duration = x.shape[0] / sample_rate
-
 waveform = tf.Variable(x,dtype=tf.float32)
 print(f"Audio shape: {waveform.shape}")
 print(f"Audio duration: {duration} seconds")
 
 # get embedding
 scores, embeddings, spectrograms = yamnet(waveform)
-
+n_windows = int(np.floor(duration/hop_size))
+print(f"N windows = {n_windows}")
 print(f"embedding shape: {embeddings.shape}")
 print(f"Embedding duration: {duration / embeddings.shape[0]}")
 
@@ -67,7 +68,14 @@ print(projected_embeddings.max())
 
 print(f"Projected embeddings shape {projected_embeddings.shape}")
 
+start_samples = [int(i*hop_size*sample_rate) for i in range(n_windows)]
+end_samples = [int((i*hop_size + window_size)*sample_rate) for i in range(n_windows)]
+
+json_dict = {"start_sample": start_samples,
+            "end_sample":end_samples,
+            "projections":projected_embeddings.tolist()}
+
 with open("projections.json","w") as json_file:
-    json.dump({"projections":projected_embeddings.tolist()},json_file)
+    json.dump(json_dict,json_file)
 
 
