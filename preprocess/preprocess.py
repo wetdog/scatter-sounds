abbbb48bae1b8390b4c84188ea307b65d4112b3b
Jose Giraldo <wetdog@Joses-MacBook-Air.local>
2022-08-31 22:36:11 +0200

    Added argument parser and flac writing

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,30 +1,37 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-import soundfile as sf
-import resampy
 import os
-from sklearn.manifold import TSNE
 import json 
-import umap
 from PIL import Image
 from matplotlib import cm
+import argparse
 
 from utils import *
 
-# load audio 
-audio_file = "../long_audio.wav"
-audio_name = os.path.basename(audio_file).split(".")[0]
+parser = argparse.ArgumentParser(description='Preprocess audio files for scatter sound viz')
+parser.add_argument("-f","--file" ,type=str)
+parser.add_argument("-d","--dir",type=str)
+args = parser.parse_args()
+output_path = "../data" 
+
+if args.file:
+    audio_file = args.file
+else:
+    audio_file = "../city_sound.wav"
+
 assert os.path.isfile(audio_file), "Invalid audio path"
 
 # Load models from TF-hub
 yamnet = hub.load('https://tfhub.dev/google/yamnet/1')
 
+audio_name = os.path.basename(audio_file).split(".")[0]
 sample_rate = 16000
 hop_size = 0.48
 window_size = 0.96
 
-#x =  get_random_signal(n_seconds=4,sample_rate=sample_rate)
+# load audio 
+# #x =  get_random_signal(n_seconds=4,sample_rate=sample_rate)
 x = load_audio_16khz(audio_file)
 duration = x.shape[0] / sample_rate
 waveform = tf.Variable(x,dtype=tf.float32)
@@ -39,11 +46,8 @@ print(f"embedding shape: {embeddings.shape}")
 print(f"Embedding duration: {duration / embeddings.shape[0]}")
 
 # dimensionality reduction 
-
-projected_embeddings = reduce_dim(method="UMAP")
+projected_embeddings = reduce_dim(embeddings, method="UMAP")
 norm_projected_embeddings = projected_embeddings / projected_embeddings.max()
-
-print(projected_embeddings.max())
 print(f"Projected embeddings shape {projected_embeddings.shape}")
 
 start_samples = [int(i*hop_size*sample_rate) for i in range(n_windows)]
@@ -55,12 +59,10 @@ json_dict = {"idx":ids,
             "end_sample":end_samples,
             "projections":projected_embeddings.tolist()}
 
-with open("projections.json","w") as json_file:
+with open(os.path.join(output_path,"projections.json"),"w") as json_file:
     json.dump(json_dict,json_file)
 
-
 ## spritesheet
-
 # create sprite image
 if type(spectrograms).__name__ != 'ndarray':
   spectrograms = spectrograms.numpy()
@@ -68,7 +70,7 @@ if type(spectrograms).__name__ != 'ndarray':
 if len(spectrograms.shape) > 2:
   spectrograms = np.squeeze(spectrograms,axis=0)
 
-img_dim = 150
+img_dim = 50
 step = int(np.floor(spectrograms.shape[0] / embeddings.shape[0]))
 spectrograms = spectrograms[:,::-1]
 spectrograms_scaled = spectrograms + np.abs(np.min(spectrograms))
@@ -90,5 +92,7 @@ for count, image in enumerate(images):
     w_loc = image_width*mod    
     spriteimage.paste(image,(w_loc,h_loc))
 
-sprite_file = f"{audio_name}_sprite.jpg"
+sprite_file = os.path.join(output_path,f"{audio_name}_sprite.jpg")
 spriteimage.convert("RGB").save(sprite_file, transparency=0)
+# write audio file
+sf.write(file=os.path.join(output_path,f"{audio_name}.flac"),data=x,samplerate=16000)
