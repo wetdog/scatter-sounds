e8c19d08edc8fe0f9da53a3790e368e4a81629db
Jose Giraldo <jose091@gmail.com>
2022-09-15 21:52:58 +0200

    update preprocess with new functions

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -4,7 +4,6 @@ import tensorflow_hub as hub
 import os
 import json 
 import argparse
-from tqdm import tqdm
 from utils import *
 
 parser = argparse.ArgumentParser(description='Preprocess audio files for scatter sound viz')
@@ -19,6 +18,7 @@ if args.file:
     audio_name = os.path.basename(audio_file).split(".")[0]
     model_sample_rate = 16000
     x = load_audio_resample(audio_file,target_sr=model_sample_rate)
+    metadata = get_metadata(x,hop_size=0.48,window_size=0.96,sr=model_sample_rate)
 
 else:
     audio_file = "../city_sound.wav"
@@ -28,34 +28,69 @@ if args.dir:
     audio_folder = args.dir
     assert os.path.isdir(audio_folder), "Invalid audio folder"
     audio_name = os.path.basename(audio_folder)
-    x = load_clips_from_folder(audio_folder,clip_dur=1,target_sr=16000)
+    x, metadata = process_clips_from_folder(audio_folder,clip_dur=0.96,global_sr=44100,target_sr=16000)
 
 # Load models from TF-hub
 yamnet = hub.load('https://tfhub.dev/google/yamnet/1')
-vggish = hub.load('https://tfhub.dev/google/vggish/1')
+#vggish = hub.load('https://tfhub.dev/google/vggish/1')
 
-
+# output file parameters
 sprite_img_dim = 150
 sprite_file = os.path.join(output_path,f"{audio_name}_sprite.jpg")
 output_audiofile = os.path.join(output_path,f"{audio_name}.flac")
-projections_file = os.path.join(output_path,"projections.json")
-config_file = os.path.join(output_path,"config.json")
+projections_file = os.path.join(output_path,f"{audio_name}_projections.json")
+config_file = os.path.join(output_path,f"{audio_name}_config.json")
 
+# model parameters
 model_sample_rate = 16000
 hop_size = 0.48
 window_size = 0.96
-
-# load audio 
-# #x =  get_random_signal(n_seconds=4,sample_rate=sample_rate)
-
+chunk_size_seconds = 900
+max_audio_duration = 3600
 duration = x.shape[0] / model_sample_rate
-waveform = tf.Variable(x,dtype=tf.float32)
-print(f"Audio shape: {waveform.shape}")
-print(f"Audio duration: {duration} seconds")
 
-# get embedding
-scores, embeddings, spectrograms = yamnet(waveform)
-n_windows = int(np.floor(duration/hop_size))
+if duration < max_audio_duration:    
+    waveform = tf.Variable(x,dtype=tf.float32)
+    print(f"Audio shape: {waveform.shape}")
+    print(f"Audio duration: {duration} seconds")
+    # get embedding
+    scores, embeddings, spectrograms = yamnet(waveform)
+
+else:
+    chunk_size_samples = int(model_sample_rate * chunk_size_seconds) # 900 seconds audio chunks
+    n_chunks = int(np.ceil(len(x)/chunk_size_samples))
+    if len(x) < n_chunks*chunk_size_samples:
+        pad_array = np.zeros(n_chunks*chunk_size_samples - len(x))
+        x = np.hstack([x,pad_array])
+        print("signal pad")
+    embeddings_list= []
+    scores_list = []
+    spectrograms_list = []
+    print(f"Audio duration: {duration} seconds")
+
+    for sample_start in np.arange(0,len(x)-chunk_size_samples,chunk_size_samples):
+        waveform = x[sample_start:sample_start+chunk_size_samples]
+        waveform = tf.Variable(waveform,dtype=tf.float32)
+        print(f"Audio shape: {waveform.shape}")
+        
+        # get embedding
+        temp_scores, temp_embeddings, temp_spectrograms = yamnet(waveform)
+        embeddings_list.append(temp_embeddings)
+        spectrograms_list.append(temp_spectrograms)
+        scores_list.append(temp_scores)
+
+    embeddings = tf.concat(embeddings_list,axis=0)
+    spectrograms = tf.concat(spectrograms_list,axis=0)
+    scores = tf.concat(scores_list,axis=0)
+
+print(f"embedding shape: {embeddings.shape}")
+if args.dir:
+    # Get rid of half window embedding for 1 second clips
+    embeddings = embeddings[::2,:]
+    n_windows = int(np.floor(duration/window_size))
+else:
+    n_windows = int(np.floor(duration/hop_size))
+
 print(f"N windows = {n_windows}")
 print(f"embedding shape: {embeddings.shape}")
 print(f"Embedding duration: {duration / embeddings.shape[0]}")
@@ -66,18 +101,13 @@ norm_projected_embeddings = np.divide(projected_embeddings, projected_embeddings
 print(f"Projected embeddings shape {projected_embeddings.shape}")
 
 # spherical projection
-embedding_magnitude = np.sqrt(np.sum(np.power(norm_projected_embeddings,2),axis=1))
+embedding_magnitude = np.sqrt(np.sum(np.power(projected_embeddings,2),axis=1))
 sphere_radius = 10
 Q = sphere_radius/embedding_magnitude
-spherical = np.multiply( Q.reshape(-1,1),norm_projected_embeddings)
+spherical = np.multiply( Q.reshape(-1,1),projected_embeddings)
 
-start_seconds = [int(i*hop_size) for i in range(n_windows)]
-end_seconds = [int((i*hop_size + window_size)) for i in range(n_windows)]
-ids = [i for i in range(n_windows)]
-
-json_dict = {"idx":ids,
-            "start_seconds": start_seconds,
-            "end_seconds":end_seconds,
+json_dict = {"idx":[i for i in range(n_windows)],
+            "metadata":metadata,
             "projections":projected_embeddings.tolist(),
             "spherical":spherical.tolist()}
 
@@ -90,14 +120,14 @@ with open(projections_file, "w") as json_file:
 spriteimage = create_spritesheet(spectrograms,n_examples=embeddings.shape[0],img_dim=sprite_img_dim)
 spriteimage.convert("RGB").save(sprite_file, transparency=0)
 
-# write audio file
+# write output flac audio file
 sf.write(file=output_audiofile, data=x, samplerate=model_sample_rate)
 
 # write config file 
-config_dict = {"audioFile":output_audiofile,
-                "spriteFile":sprite_file,
-                "imgDim":sprite_img_dim,
-                "projectionsFile":projections_file}
+config_dict = {"audioFile":output_audiofile[1:],
+                "spriteFile":sprite_file[1:],
+                "projectionsFile":projections_file[1:],
+                "imgDim":sprite_img_dim}
 
 with open(config_file, "w") as json_file:
     json.dump(config_dict,json_file)
